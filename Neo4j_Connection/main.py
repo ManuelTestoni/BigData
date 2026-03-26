a9fe596ab30e3f94e6fa4ed3d4a51db32f544e1c
318215 <318215@studenti.unimore.it>
2026-03-26 10:24:03 +0100

    fixing notes

--- Neo4j_Connection/main.py
+++ Neo4j_Connection/main.py
@@ -1,6 +1,5 @@
 from neo4j_conn import get_neo4j_driver
 
-# Esempio di una funzione che fa la query passando il driver connesso
 def fetch_some_data(driver):
     with driver.session() as session:
         result = session.run("MATCH (n) RETURN n LIMIT 5")
@@ -8,12 +7,12 @@ def fetch_some_data(driver):
             print(record)
 
 if __name__ == "__main__":
-    # 1. Recupera la connessione al database
+    # 1. get the connection
     my_driver = get_neo4j_driver()
     
     try:
-        # 2. Fai le tue query
+        # 2. Here we will perform our queries
         fetch_some_data(my_driver)
     finally:
-        # 3. Assicurati di chiudere il driver alla fine di tutto!
+        # 3. Closing connection
         my_driver.close()
