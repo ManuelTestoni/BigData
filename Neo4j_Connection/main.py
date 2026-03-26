66618b6baf474d65c6e69973637e6ebda6e4a77e
318215 <318215@studenti.unimore.it>
2026-03-26 10:44:50 +0100

    first query test

--- Neo4j_Connection/main.py
+++ Neo4j_Connection/main.py
@@ -1,18 +1,28 @@
 from neo4j_conn import get_neo4j_driver
 
 def fetch_some_data(driver):
-    with driver.session() as session:
-        result = session.run("MATCH (n) RETURN n LIMIT 5")
-        for record in result:
-            print(record)
+    # Get the names of people born in a specific year (e.g. 1964)
+    records, summary, keys = driver.execute_query("MATCH (p:Person {birthYear: $birthYear}) RETURN " \
+    "p.name AS name", birthYear=1964, database_="movies",) 
+    # Loop through results and do something with them 
+    for person in records:  
+        print(person) 
+    # Summary information 
+    print("The query '{query}' returned {records_count} records in {time} ms." 
+          .format(query=summary.query, records_count=len(records), time=summary.result_available_after, 
+        ))
 
 if __name__ == "__main__":
     # 1. get the connection
     my_driver = get_neo4j_driver()
+    print("Connected Successfully to Local Server")
     
     try:
         # 2. Here we will perform our queries
+        print("--------------------------------------------------")
         fetch_some_data(my_driver)
+        print("--------------------------------------------------")
     finally:
         # 3. Closing connection
         my_driver.close()
+        print("Connection closed successfully!")
